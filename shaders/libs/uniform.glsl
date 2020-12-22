51d21fc0332a39caf3c40537c46530e9928881f8
pspupsp <pspupsp@hypercol.xyz>
2020-12-22 08:54:08 +0000

    Added a uniform variable for controling the specific effects in shaders

--- shaders/libs/uniform.glsl
+++ shaders/libs/uniform.glsl
@@ -8,6 +8,8 @@ uniform float viewHeight;                       // viewHeight
 uniform vec2 pixel;
 #endif
 
+uniform float screenBrightness;                 //screen brightness (0.0-1.0)
+
 uniform float near;
 uniform float far;
 
